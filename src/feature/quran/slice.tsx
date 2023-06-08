89556fb3ba8e6799414bf7955e2056cac93a9234
suraji <surajidk12@gmail.com>
2023-06-08 21:14:11 +0700

    fix bug loading

--- src/feature/quran/slice.tsx
+++ src/feature/quran/slice.tsx
@@ -29,7 +29,7 @@ const quranSlice = createSlice({
       state.loading = false;
     });
     builder.addCase(detailSurah.pending, (state) => {
-      state.loading = false;
+      state.loading = true;
     });
   },
 });
